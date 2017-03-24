62888274941052b4260cf6141d14d73b8d46632f
Lizeth1800 <liset_nestor@hotmail.com>
2017-03-23 18:53:21 -0600

    se agrego 3._numerosimpares al repositorio

---
+++ 2._Volumendeesfera.py
@@ -0,0 +1,4 @@
+import math
+radio_esfera=float(input("Ingrese el radio de la esfera: "))
+volumen_esfera=(math.pi * radio_esfera**2) / 3
+print ("El volumen de la esfera es:",volumen_esfera)
---
+++ 3._numerosimpares.py
@@ -0,0 +1,8 @@
+###Imprimir los numeros impares desde el 1 al 25, ambos inclusive
+n = 13
+h = ''
+while n <= 32:
+    if n%2 != 0:
+        h += ' %i' % n
+    n += 1
+print (h)
