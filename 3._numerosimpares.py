11c2e4ac06ace6b98d2634e013e0d60b93520d07
Lizeth1800 <liset_nestor@hotmail.com>
2017-03-26 21:31:53 -0600

    modificados

--- 3._numerosimpares.py
+++ 3._numerosimpares.py
@@ -1,8 +1,10 @@
 ###Imprimir los numeros impares desde el 1 al 25, ambos inclusive
-n = 13
-h = ''
-while n <= 32:
-    if n%2 != 0:
-        h += ' %i' % n
-    n += 1
-print (h)
+def impar():
+    A = 13
+    h = 1
+    n = int(input("N impares iniciando con 13: "))
+    while h <= n:
+        h+=1
+        print(A)
+        A+=2
+impar()
--- 4._mayorde3.py
+++ 4._mayorde3.py
@@ -1,6 +1,11 @@
+def cuatro():
+   A=0
+   B=0
+   C=0
 A=int(input("ingrese un numero\n"))
 B=int(input("ingrese otro numero\n"))
 C=int(input("ingrese un nuemero\n"))
+
 if(A > B and A > C):
  print("El numero mayor es " + str(A))
 else:
@@ -8,3 +13,4 @@ else:
   print("El numero mayor es " + str(B))
  else:
   print("El numero mayor es " + str(C))
+cuatro()
